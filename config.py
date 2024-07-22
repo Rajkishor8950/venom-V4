a451f86cf434a9a2784591fecb182a3b0cd6eff9
Rajkishor8950 <kumarrajkishor8950@gmail.com>
2024-07-22 05:52:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7271692745:AAEVmTOxSGLfXvLepbSdG117xq6hIlc1LdA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Rajkishor8950")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1414983675")
 
 
 
